f6a6674d5d58f62997dd12cf1a7d1defc7b36c99
Umidbahromov1634 <140816390+Umidbahromov1634@users.noreply.github.com>
2025-09-24 12:33:48 +0500

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const ROUTE_EMAIL = process.env.ROUTE_EMAIL || "umidbahromov24_gmail_com";
 function gcd(a, b) {
   return b === 0 ? a : gcd(b, a % b);
 }
-
 function lcm(x, y) {
   return (x * y) / gcd(x, y);
 }
@@ -18,25 +17,22 @@ app.get(`/${ROUTE_EMAIL}`, (req, res) => {
   let x = parseInt(req.query.x, 10);
   let y = parseInt(req.query.y, 10);
 
-  if (isNaN(x) || isNaN(y)) {
-    return res.send("NaN");
+  if (isNaN(x) || isNaN(y) || x <= 0 || y <= 0) {
+    return res.end("NaN");
   }
 
-  if (x <= 0 || y <= 0) {
-    return res.send("NaN");
-  }
-
-  return res.send(lcm(x, y).toString());
+  return res.end(lcm(x, y).toString());
 });
 
 app.get(`/${ROUTE_EMAIL}*`, (req, res) => {
   res.setHeader("Content-Type", "text/plain; charset=utf-8");
-  res.send("NaN");
+  res.end("NaN");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 
 
